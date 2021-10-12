a62877af3b72849f60bed0d2784eca4e3f394ad8
JSvoldgaard <Jacob.svoldgaard93@gmail.com>
2021-10-12 22:52:56 +0200

    Creditcard details done except for delete api-hookup

--- Swafe1/src/app/modules/credit-card/credit-card-detail/credit-card-detail.component.ts
+++ Swafe1/src/app/modules/credit-card/credit-card-detail/credit-card-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CreditCard } from 'src/app/models/creditcard';
+import { Transaction } from 'src/app/models/transaction';
+import { TransactionService } from '../../transaction/transaction.service';
 import { CreditCardService } from '../credit-card.service';
-
+import { filter } from 'rxjs/operators'
 @Component({
   selector: 'app-credit-card-detail',
   templateUrl: './credit-card-detail.component.html',
@@ -11,21 +13,26 @@ import { CreditCardService } from '../credit-card.service';
 export class CreditCardDetailComponent implements OnInit {
 
   creditCard!: CreditCard
+  transactionList!: Transaction[]
 
-  constructor(private activatedRoute: ActivatedRoute, private cardService:CreditCardService) { } //@Inject(CREDITCARDS) private creditCards
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private cardService:CreditCardService,
+    private transactionService: TransactionService) { }
 
   ngOnInit(): void {
     var number = Number(this.activatedRoute.snapshot.paramMap.get('card_number'))
     console.log(number)
     this.cardService.getCreditCards().subscribe(res => {
       var list = res
-      var gaming = list.find(c => c.card_number === number)
-      if(gaming){
-        this.creditCard = gaming
+      var specificCard = list.find(c => c.card_number === number)
+      if(specificCard){
+        this.creditCard = specificCard
         console.log(this.creditCard)
+        this.getTransactions(this.creditCard)
       }
     })
-    
+ 
     // this.activatedRoute.data.subscribe(d => console.log(d))
     // let x = this.activatedRoute.snapshot.paramMap.get('data')
     //this.activatedRoute.paramMap.subscribe(d =>console.log(d))
@@ -38,4 +45,19 @@ export class CreditCardDetailComponent implements OnInit {
     // console.log(x)
   }
 
+  getTransactions(card: CreditCard) {
+    this.transactionService.getTransactions().subscribe(res => {
+      var list = res
+      var specificTransactions = list.filter(c => c.credit_card.card_number === card.card_number) //(c => c.credit_card === card)
+      // console.log(specificTransactions)
+      if(specificTransactions) {
+        this.transactionList = specificTransactions
+        console.log(this.transactionList)
+      }
+    })
+  }
+
+  deleteCard(card: CreditCard) {
+    console.log(card)
+  }
 }
